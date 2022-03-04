8708af69995b0117d6665a446f26a8cae705a59a
Jon Chirinos <chirinosjon@gmail.com>
2022-03-04 00:43:07 -0600

    finished establishing routes for gets/posts

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -1,9 +1,34 @@
 // create post route
 const fb = require("express").Router();
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const { readAndAppend, readFromFile } = require("../../helpers/fsUtils");
 
-fb.get("/", (req, res) => readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data))));
+fb.get("/notes", (req, res) => readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data))));
 // create get route to show the notes in the form of an array
+fb.post("/notes", (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+            feedback_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, "./db/db.json");
+
+        const response = {
+            status: "success",
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json("Error in posting feedback");
+    }
+});
 // EXTRA CREDIT delete the notes
 module.exports = fb;
